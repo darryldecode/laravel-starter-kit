6594ce2c70f4d414c10c290a72c1c374342cd1fc
darryl fernandez <engrdarrylfernandez@gmail.com>
2017-10-31 01:29:15 -0700

    Fix namespace error cause by previous refactoring

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Components\User\Models\User::class, function (Faker $faker) {
     static $password;
 
     $permissionKeys = [
