5cc6388c7bf06e558194a99b2605de849a1cc054
Darryldecode <engrdarrylfernandez@gmail.com>
2018-09-25 19:48:53 +0800

    updated vuetify to latest version and some typo fixes

--- resources/js/admin/dashboard/Home.vue
+++ resources/js/admin/dashboard/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="page_wrap_vue pa-3">
         <h2>HOME PAGE</h2>
-        <p>I am located at <b>resources/assets/js/admin/pages/dashboard/Home.vue</b></p>
-        <p>single page routes can be found at <b>resources/assets/js/router</b></p>
+        <p>I am located at <b>resources/assets/js/admin/dashboard/Home.vue</b></p>
+        <p>single page routes for admin can be found at <b>resources/js/admin/router.js</b></p>
     </div>
 </template>
 
--- resources/js/admin/settings/Settings.vue
+++ resources/js/admin/settings/Settings.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="page_wrap_vue pa-3">
         <h2>SETTINGS PAGE</h2>
-        <p>I am located at <b>resources/assets/js/admin/pages/settings/Settings.vue</b></p>
-        <p>single page routes can be found at <b>resources/assets/js/router</b></p>
+        <p>I am located at <b>resources/assets/js/admin/settings/Settings.vue</b></p>
+        <p>single page routes for admin can be found at <b>resources/js/admin/router.js</b></p>
     </div>
 </template>
 
