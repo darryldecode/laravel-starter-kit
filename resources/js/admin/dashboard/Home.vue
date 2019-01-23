e2cfad78f738f328d3de62d776b87b849b449d22
Darryldecode <engrdarrylfernandez@gmail.com>
2019-01-23 10:34:50 +0800

    small details fixes

--- resources/js/admin/dashboard/Home.vue
+++ resources/js/admin/dashboard/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page_wrap_vue pa-3">
         <h2>HOME PAGE</h2>
-        <p>I am located at <b>resources/assets/js/admin/dashboard/Home.vue</b></p>
+        <p>I am located at <b>resources/js/admin/dashboard/Home.vue</b></p>
         <p>single page routes for admin can be found at <b>resources/js/admin/router.js</b></p>
     </div>
 </template>
@@ -9,8 +9,7 @@
 <script>
     export default {
         mounted() {
-            console.log('pages.Home.vue');
-
+            
             const self = this;
 
             self.$store.commit('setBreadcrumbs',[
--- resources/js/admin/settings/Settings.vue
+++ resources/js/admin/settings/Settings.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page_wrap_vue pa-3">
         <h2>SETTINGS PAGE</h2>
-        <p>I am located at <b>resources/assets/js/admin/settings/Settings.vue</b></p>
+        <p>I am located at <b>resources/js/admin/settings/Settings.vue</b></p>
         <p>single page routes for admin can be found at <b>resources/js/admin/router.js</b></p>
     </div>
 </template>
@@ -9,7 +9,6 @@
 <script>
     export default {
         mounted() {
-            console.log('pages.Settings.vue');
 
             const self = this;
 
