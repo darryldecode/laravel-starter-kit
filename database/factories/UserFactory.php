69fcbb024dbaf355e4efcae32c5419819bad2dea
Darryldecode <engrdarrylfernandez@gmail.com>
2019-10-29 19:42:27 +0800

    fix str_random error in UserFactory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,7 @@ $factory->define(App\Components\User\Models\User::class, function (Faker $faker)
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '12345678',
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
         'permissions' => [],
         'last_login' => $faker->dateTime,
         'active' => null,
