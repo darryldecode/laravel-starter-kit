870edd791ce5c923ad997ed2ae466a57c43e2692
esquinas <esquinas.enrique@gmail.com>
2019-12-13 13:46:43 +0100

    Minor alignment fixes.

--- resources/js/admin/users/components/UserLists.vue
+++ resources/js/admin/users/components/UserLists.vue
@@ -57,12 +57,12 @@
                 <thead>
                 <tr>
                     <th v-for="header in headers">
-                        <span v-if="header.value=='name'"><v-icon>mdi-person</v-icon> {{header.text}}</span>
-                        <span v-else-if="header.value=='email'"><v-icon>mdi-email</v-icon> {{header.text}}</span>
-                        <span v-else-if="header.value=='permissions'"><v-icon>mdi-vpn_key</v-icon> {{header.text}}</span>
-                        <span v-else-if="header.value=='groups'"><v-icon>mdi-group</v-icon> {{header.text}}</span>
-                        <span v-else-if="header.value=='last_login'"><v-icon>mdi-av_timer</v-icon> {{header.text}}</span>
-                        <span v-else>{{header.text}}</span>
+                        <div v-if="header.value=='name'" :class="`text-${header.align}`"><v-icon>mdi-person</v-icon> {{header.text}}</div>
+                        <div v-else-if="header.value=='email'" :class="`text-${header.align}`"><v-icon>mdi-email</v-icon> {{header.text}}</div>
+                        <div v-else-if="header.value=='permissions'" :class="`text-${header.align}`"><v-icon>mdi-vpn_key</v-icon> {{header.text}}</div>
+                        <div v-else-if="header.value=='groups'" :class="`text-${header.align}`"><v-icon>mdi-group</v-icon> {{header.text}}</div>
+                        <div v-else-if="header.value=='last_login'" :class="`text-${header.align}`"><v-icon>mdi-av_timer</v-icon> {{header.text}}</div>
+                        <div v-else :class="`text-${header.align}`">{{header.text}}</div>
                     </th>
                 </tr>
                 </thead>
@@ -71,12 +71,12 @@
                 <tbody>
                     <tr v-for="item in items" :key="item.id">
                         <td>
-                            <div class="text-center">
-                                <v-btn @click="$router.push({name:'users.edit',params:{id: item.id}})" class="ma-2" outlined fab small color="info">
-                                    <v-icon>mdi-pencil</v-icon>
+                            <div class="ml-n1 my-1 d-flex justify-space-between align-content-space-around flex-wrap">
+                                <v-btn @click="$router.push({name:'users.edit',params:{id: item.id}})" class="ma-1" small outlined icon color="info">
+                                    <v-icon small>mdi-pencil</v-icon>
                                 </v-btn>
-                                <v-btn @click="trash(item)" class="ma-2" outlined fab small color="red">
-                                    <v-icon>mdi-delete</v-icon>
+                                <v-btn @click="trash(item)" class="ma-1" small outlined icon color="red">
+                                    <v-icon small>mdi-delete</v-icon>
                                 </v-btn>
                             </div>
                         </td>
@@ -91,7 +91,7 @@
                             </v-chip>
                         </td>
                         <td>{{ $appFormatters.formatDate(item.last_login) }}</td>
-                        <td>
+                        <td class="text-center">
                             <v-avatar outlined>
                                 <v-icon v-if="item.active!=null" class="green--text">check_circle</v-icon>
                                 <v-icon class="grey--text" v-else>error_outline</v-icon>
@@ -142,7 +142,7 @@
                     { text: 'Permissions', value: 'permissions', align: 'left', sortable: false },
                     { text: 'Groups', value: 'groups', align: 'left', sortable: false },
                     { text: 'Last Login', value: 'last_login', align: 'left', sortable: false },
-                    { text: 'Active', value: 'active', align: 'left', sortable: false },
+                    { text: 'Active', value: 'active', align: 'center', sortable: false },
                 ],
                 items: [],
                 totalItems: 0,
